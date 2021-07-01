979e368d900a9aa7bf2c76540f8841c91a2c6114
Chris Kennedy <chris@thekennedystn.com>
2021-07-01 18:13:30 -0400

    Command to open ports on hosts for portworx

---
+++ tanzu-cluster/px-photonos-fw.sh
@@ -0,0 +1,6 @@
+kubectl get pods -n kube-system -l name=portworx |cut -f1 -d\  |\
+while read pod; \
+ do echo "$pod setting host firewall rules:";\
+  kubectl exec -t $pod -n kube-system -- nsenter --mount=/host_proc/1/ns/mnt bash -c \
+   "iptables -A INPUT -p tcp --match multiport --dports 9001:9020 -j ACCEPT && iptables -A INPUT -p tcp --match multiport --dports 1970 -j ACCEPT"
+done
