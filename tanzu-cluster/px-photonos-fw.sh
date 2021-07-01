63f48a0db60442c8bd758c3d87bd24c5cc3a196a
Chris Kennedy <chris@thekennedystn.com>
2021-07-01 18:35:30 -0400

    Script for psp and iptables

--- tanzu-cluster/px-photonos-fw.sh
+++ tanzu-cluster/px-photonos-fw.sh
@@ -1,4 +1,8 @@
-kubectl get pods -n kube-system -l name=portworx |cut -f1 -d\  |\
+kubectl create clusterrolebinding default-tkg-admin-privileged-binding\
+ --clusterrole=psp:vmware-system-privileged --group=system:authenticated &&\
+ kubectl create rolebinding portworx-cluster-rolebinding --namespace=kube-system\
+  --clusterrole=psp:vmware-system-privileged --group=system:serviceaccounts &&\
+ kubectl get pods -n kube-system -l name=portworx |cut -f1 -d\  |\
 while read pod; \
  do echo "$pod setting host firewall rules:";\
   kubectl exec -t $pod -n kube-system -- nsenter --mount=/host_proc/1/ns/mnt bash -c \
